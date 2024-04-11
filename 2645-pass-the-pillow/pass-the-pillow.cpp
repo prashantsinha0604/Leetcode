012c5ec16cc3dccb6bd363eefe465809e2df8cb1
prashantsinha0604 <157583638+prashantsinha0604@users.noreply.github.com>
2024-04-11 11:43:04 +0530

    Time: 3 ms (23.21%) | Memory: 7.2 MB (36.61%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-       int person = 1 ; 
-       bool flag = false ;
-       if ( time < n )
-       {
-        return ( time + 1 );
-       }
-       else
-       {
-            for( int i = 1 ; i <= time ; i++ )
-            {
-                if ( i >= n and flag == false  )
-                {
-                    person--;
-                    if ( person == 1 )
-                    {
-                        flag = true ;
-                    }
-                }
-                else if ( flag == true and i > n  )
-                {
-                    person++;
-                    if ( person == n )
-                    {
-                        flag = false;
-                    }
-                }
-                else
-                {
-                    person++;
-                }
-            }
-        }
-        return person;
+        int r , q ;
+        q = time / ( n - 1 );
+        r = time % (n-1) ;
+
+        if ( q & 1 )
+        {
+            return ( n - r );
+        } 
+        return ( 1 + r );
     }
 };
