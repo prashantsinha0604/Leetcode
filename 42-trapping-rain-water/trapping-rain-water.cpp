7ca4252127d735d0163c4cd9d24aed443615f3f7
prashantsinha0604 <157583638+prashantsinha0604@users.noreply.github.com>
2024-04-13 12:46:09 +0530

    Time: 15 ms (21.18%) | Memory: 23.1 MB (25.33%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
     int trap(vector<int>& h) {
+        
         int n = h.size();
-
-        vector< int > left(n), right(n);
+        vector<int > left(n) , right(n);
         left[0] = h[0];
-        right[n - 1] = h[n - 1];
+        right[ n-1 ] = h[n-1];
 
-        for(int i = 1; i < n; i++ )
+        for(int i = 1; i < n ; i++ )
         {
-            if( h[i] > left[ i - 1] )
+            if ( h[i] > left[i - 1 ] )
             {
                 left[i] = h[i];
-            }
+            } 
             else
             {
-                left[i] = left[i - 1];
+                left[i] = left[i - 1 ];
             }
         }
 
 
-        for(int i = n - 2; i > -1; i-- )
+        for(int i = n - 2 ; i > -1 ; i--)
         {
-            if( h[i] > right[i + 1] )
+            if ( h[i] > right[i +1] )
             {
                 right[i] = h[i];
             }
             else
             {
-                right[i] = right[i + 1];
+                right[i] = right[i +1 ];
             }
         }
 
-        int ans = 0;
-
-        for(int i = 1; i < n - 1; i++ )
+            int ans = 0 ;
+        for(int i = 1 ; i < n-1 ; i++ )
         {
-            if( left[i - 1] > h[i] and right[i + 1] > h[i] )
+            if ( h[i] < left[i - 1] and h[i] < right[i + 1 ] )
             {
-                ans = ans + min( left[i - 1] - h[i], right[i + 1] - h[i] );
+                ans = ans + min(left[i -1] - h[i] , right[i+1] - h[i] );
             }
         }
+        return ans ;
 
-        return ans;
     }
 };
