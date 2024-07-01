46d47530b64a0758189889a2b885c573cdbd502c
prashantsinha0604 <157583638+prashantsinha0604@users.noreply.github.com>
2024-07-01 22:31:19 +0530

    Time: 2 ms (75.17%) | Memory: 13.6 MB (7.92%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
+    
+    int solve( vector<int> nums , int low , int high , int k , int & ans )
+    {
+        if( low > high )
+        {
+            return ans ;
+        }
+        int mid = low + ( high - low )/2;
+
+        if( nums[mid] >= k )
+        {
+            ans = mid ; 
+            high = mid - 1 ;
+            return solve( nums , low , high , k , ans );
+        }
+        else 
+        {
+            low = mid + 1 ; 
+            return solve( nums , low , high , k , ans );
+        }
+    }
+
+
     int searchInsert(vector<int>& nums, int target) {
         int low , high , mid , ans ;
         ans = nums.size() , low = 0 , high = nums.size() - 1;
 
-        /* it is a lower bound which is smaller in an array 
-           such that arr[mid] >= target */
-        while( low <= high )
-        {
-            mid = low + ( high - low )/2;
-            if( nums[mid] >= target )
-            {
-                ans = mid ;
-                high = mid - 1 ;
-            }
-            else 
-            {
-                low = mid + 1  ;
-            }
-        }
-
-        return ans ;
+        return solve( nums , low , high , target , ans );
     }
 };
