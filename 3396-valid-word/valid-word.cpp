6c8b246f5d31ccc083cd2909a938c982ca9e304d
prashantsinha0604 <157583638+prashantsinha0604@users.noreply.github.com>
2024-07-07 11:15:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (11.20%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -1,57 +1,49 @@
 class Solution {
 public:
     bool isValid(string word) {
-        if( word.length() < 3)
-        {
-            return false ;
-        }
-        unordered_map < char , int > mp ;
-        int cnt_vowel = 0  , cnt_dig = 0  , cnt = 0 ;
+        int n = word.size();
 
-
-        for( int i = 0 ; i < word.length() ; i++ )
+        if( n < 3 )
         {
-            if(word[i] == 'a' or word[i] == 'e' or word[i] == 'i' or word[i] == 'o' or word[i]=='u' )
-            {
-                cnt_vowel++;
-                mp[word[i] ]++;
-            }
-            else if(word[i]== 'A' or word[i] == 'E' or word[i] == 'I' or word[i] == 'O' or word[i]=='U' )
-            {
-                cnt_vowel++;
-                mp[word[i] ]++;
-            }
-            else if ( word[i] >= 'a' and word[i] <= 'z' or word[i] >= 'A' and word[i] <= 'Z' )
-            {
-                if( mp.find(word[i]) == mp.end() )
-                {
-                    cnt++;
-                }    
-            }
-            else if ( word[i] >= '0' and word[i] <= '9')
-            {
-                cnt_dig++ ;
-            }
-            else
-            {
-                return false ;
-            }
+            return false;
         }
 
-        if( cnt_vowel == 0 )
+        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        int df = false, uf = false, vf = false, cf = false, lf = false;
+
+        for(char c: word )
         {
-            return false ;
-        }
-        // else if( cnt_dig == 0 )
-        // {
-        //     return false ;
-        // }
-        else if( cnt == 0 )
-        {
-            return false ;
+            if( !isdigit(c) and !isalpha(c) )
+            {
+                return false;
+            }
+
+            if( isdigit(c) )
+            {
+                df = true;
+            }
+
+            if( islower(c) )
+            {
+                lf = true;
+            }
+
+            if( isupper(c) )
+            {
+                uf = true;
+            }
+
+            if( vowels.count(c) )
+            {
+                vf = true;
+            }
+            else if( !vowels.count(c) and isalpha(c) )
+            {
+                cf = true;
+            }
         }
 
-        return true;
-        
+        return ( df or lf or uf ) and vf and cf;
     }
+
 };
