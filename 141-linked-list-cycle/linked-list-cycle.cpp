36a95f2c16542d15d0b8966c975af87d9196c521
prashantsinha0604 <157583638+prashantsinha0604@users.noreply.github.com>
2024-09-05 11:56:22 +0530

    Time: 16 ms (11.34%) | Memory: 15.2 MB (6.88%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,20 +9,19 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode * slow , * fast ;
-        slow = head ;
-        fast = head ;
+        unordered_map < ListNode * , int > mp ;
+        ListNode * i = head ;
 
-        while( fast and fast -> next )
+        while( i )
         {
-            slow = slow -> next ;
-            fast = fast -> next -> next ;
-
-            if( slow == fast )
+            if( mp.find( i ) != mp.end() )
             {
                 return true ;
             }
+            mp[i] = 1 ;
+            i = i -> next ;
         }
+
         return false ;
     }
 };
