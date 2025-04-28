ed24558252c600a824ab9393b7b2d47439bda0aa
prashantsinha0604 <157583638+prashantsinha0604@users.noreply.github.com>
2025-04-28 18:36:52 +0530

    Time: 7 ms (14.07%) | Memory: 14.6 MB (13.29%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+ int thirdMax(vector<int>& nums) {
+           set<int>s;
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
+        }
+        if(s.size()>=3)
+        {   
+        // when set size >=3 means 3rd Maximum exist
+        // (because set does not contain duplicate element)
+            int Third_index_from_last=s.size()-3;
+            auto third_maximum=next(s.begin(),Third_index_from_last);
+            return *third_maximum;
+        }
+            return *--s.end(); // return maximum if 3rd maximum not exist
+    }
+};
